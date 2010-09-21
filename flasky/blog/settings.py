4a104f87859488ab0495cf6dc013aaf0daac3e4b
Mark Renton <indexofire@gmail.com>
2010-09-21 08:35:45 +0800

    update

--- flasky/blog/settings.py
+++ flasky/blog/settings.py
@@ -4,5 +4,5 @@ CSRF_ENABLED      = True
 SECRET_KEY        = '64Qj&f62Fa(fa&_A98a0-1ZlkfFaGz9A$69'
 CSRF_SESSION_LKEY = '69JFJ^$(D#!S;LKdeh8asSNJ283403808=+'
 USERNAME          = 'admin'
-PASSWORD          = '123456'
+PASSWORD          = 'indexofire'
 ARTICLE_PERPAGE   = 20
